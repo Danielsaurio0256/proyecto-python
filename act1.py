35dd266c910bfd41478989192ffe207437ce5785
Danielsaurio0256 <1024591178@institutotecnisistemas.edu.co>
2025-08-04 17:11:46 -0500

    trabajo finalizado

--- act1.py
+++ act1.py
@@ -1,19 +1,122 @@
+#Python Casting
+#Specify a Variable Type
+#ejemplos
 x = int(1)   # x will be 1
 y = int(2.8) # y will be 2
 z = int("3") # z will be 3
-#segundo
+
 x = float(1)     # x will be 1.0
 y = float(2.8)   # y will be 2.8
 z = float("3")   # z will be 3.0
 w = float("4.2") # w will be 4.2
-#tercero
+
 x = str("s1") # x will be 's1'
 y = str(2)    # y will be '2'
 z = str(3.0)  # z will be '3.0' 
-#cuarto
+#Python Strings
 print("Hello")
 print('Hello')
-#quinto
+
 print("It's alright")
 print("He is called 'Johnny'")
 print('He is called "Johnny"')
+
+a = "Hello"
+print(a)
+
+a = """Lorem ipsum dolor sit amet,
+consectetur adipiscing elit,
+sed do eiusmod tempor incididunt
+ut labore et dolore magna aliqua."""
+print(a) 
+
+a = '''Lorem ipsum dolor sit amet,
+consectetur adipiscing elit,
+sed do eiusmod tempor incididunt
+ut labore et dolore magna aliqua.'''
+print(a)
+
+a = "Hello, World!"
+print(a[1])
+
+for x in "banana":
+  print(x)
+  
+  a = "Hello, World!"
+print(len(a))
+
+txt = "The best things in life are free!"
+print("free" in txt)
+
+txt = "The best things in life are free!"
+if "free" in txt:
+  print("Yes, 'free' is present.")
+  
+  txt = "The best things in life are free!"
+print("expensive" not in txt)
+
+txt = "The best things in life are free!"
+if "expensive" not in txt:
+  print("No, 'expensive' is NOT present.")
+
+b = "Hello, World!"
+print(b[2:5])
+
+b = "Hello, World!"
+print(b[:5])
+
+b = "Hello, World!"
+print(b[2:])
+
+b = "Hello, World!"
+print(b[-5:-2])
+
+a = "Hello, World!"
+print(a.upper())
+
+a = "Hello, World!"
+print(a.lower())
+
+a = " Hello, World! "
+print(a.strip()) # returns "Hello, World!" 
+
+a = "Hello, World!"
+print(a.replace("H", "J"))
+
+a = "Hello, World!"
+print(a.split(",")) # returns ['Hello', ' World!'] 
+
+a = "Hello"
+b = "World"
+c = a + b
+print(c)
+
+a = "Hello"
+b = "World"
+c = a + " " + b
+print(c)
+
+age = 36
+#This will produce an error:
+txt = "My name is John, I am +age"
+print(txt)
+
+age = 36
+txt = f"My name is John, I am {age}"
+print(txt)
+
+price = 59
+txt = f"The price is {price} dollars"
+print(txt)
+
+price = 59
+txt = f"The price is {price:.2f} dollars"
+print(txt)
+
+txt = f"The price is {20 * 59} dollars"
+print(txt)
+
+txt = "We are the so-called Vikings from the north."
+
+txt = "We are the so-called \"Vikings\" from the north."
+
