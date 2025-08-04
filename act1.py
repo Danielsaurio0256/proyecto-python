16dd7a2e9220788814e799fec15d6192875f6025
Danielsaurio0256 <1024591178@institutotecnisistemas.edu.co>
2025-08-04 16:01:22 -0500

    primer ejercicio

---
+++ act1.py
@@ -0,0 +1,19 @@
+x = int(1)   # x will be 1
+y = int(2.8) # y will be 2
+z = int("3") # z will be 3
+#segundo
+x = float(1)     # x will be 1.0
+y = float(2.8)   # y will be 2.8
+z = float("3")   # z will be 3.0
+w = float("4.2") # w will be 4.2
+#tercero
+x = str("s1") # x will be 's1'
+y = str(2)    # y will be '2'
+z = str(3.0)  # z will be '3.0' 
+#cuarto
+print("Hello")
+print('Hello')
+#quinto
+print("It's alright")
+print("He is called 'Johnny'")
+print('He is called "Johnny"')
